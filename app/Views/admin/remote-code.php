2c4b5b5fae5a55d288fcfaa990b2f0179ccc52eb
zhyea <zhangrui@zhyea.com>
2022-03-04 12:33:35 +0800

    迁移中-13: 调整权限、限流、远程码等功能

--- app/Views/admin/remote-code.php
+++ app/Views/admin/remote-code.php
@@ -4,6 +4,18 @@
         <h3><i class="glyphicon glyphicon-cloud"></i> 远程写</h3>
     </div>
 
-    <p>交互码为：<strong><?= $code ?></strong></p>
-    <p>交互码过期时间为：<strong><?= $expire_time ?></strong></p>
+    <form>
+        <div class="row">
+            <div class="form-label col-md-2 col-xs-12">交互码</div>
+            <div class="form-input col-md-10 col-xs-12">
+                <input type="text" class="form-control" name="id" value="<?= $code ?>" readonly/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-label col-md-2 col-xs-12">过期时间</div>
+            <div class="form-input col-md-10 col-xs-12">
+                <input type="text" class="form-control" name="id" value="<?= $expire_time ?>" readonly/>
+            </div>
+        </div>
+    </form>
 </div>
