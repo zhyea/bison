e8d03aaa426d26b81358e2297eb103ec54daed01
zhyea <zhangrui@zhyea.com>
2022-04-21 11:09:37 +0800

    调整评论维护功能

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -7,11 +7,25 @@
     <?php if (!empty($comments)) { ?>
         <?php foreach ($comments as $c) { ?>
             <div class="row comments">
-                <div class="comment-user">
-                    <div class="comment-icon"><img src="<?= $uriTheme ?>/imgs/avatar.jpg"></div>
+                <div class="comment-work">
+                    <a><?= $c['work_name'] ?></a>
+                    <?php if (!empty($c['chapter_id']) && 0 != $c['chapter_id']) { ?>
+                        / <a><?= $c['chapter_name'] ?></a>
+                    <?php } ?>
                 </div>
                 <div class="comment-name"><?= $c['name'] ?></div>
-                <div class="comment-content"><?= $c['content'] ?></div>
+                <div class="comment-content">
+                    <?= $c['content'] ?>
+                    <div class="comment-operate">
+                        <div class="comment-delete">
+                            <a>删除</a>
+                        </div>
+                        <div class="comment-approve">
+                            <a>批准</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         <?php } ?>
     <?php } ?>
